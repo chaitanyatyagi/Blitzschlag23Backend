163c990e5b08eeaebdcab72190f5ecf757db673b
Vishnu Mali <vishnumali3911@gmail.com>
2023-01-06 00:42:54 +0530

    registration added

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,13 @@
 const User = require("../model/userModel")
-
+const Registration=require("../model/registrations");
 exports.getUser = async (req, res, next) => {
     const user = await User.findById(req.user._id)
+    const registeredEvents=await Registration.find( {userId:user.blitzId})
     var allowedDomains = ["http://localhost:3000","https://blitzschlag.co.in"];
     var origin = req.headers.origin;
     if(allowedDomains.indexOf(origin) > -1){
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
     res.set('Access-Control-Allow-Credentials', 'true')
-    return res.json(user)
+    return res.json({...user._doc,events:registeredEvents});
 }
---
+++ model/registrations.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const registrationSchema = new mongoose.Schema({
+    eventName:String,
+    userId:String,
+    teamName:String,
+    members:Number,
+    registrationTime:Date,
+    payment:{paid:{type:Boolean,default:false},TransactionId:String,timeStamp:Date}
+})
+
+const Registration = mongoose.model('Registration', registrationSchema)
+module.exports = Registration 
